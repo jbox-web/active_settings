8bcdcc32cb79ce474492ebf665b38c8cf5cae21c
Nicolas Rodriguez <nicoladmin@free.fr>
2019-10-11 01:49:06 +0200

    Small fix for ruby-head

--- lib/active_settings/config.rb
+++ lib/active_settings/config.rb
@@ -90,7 +90,7 @@ module ActiveSettings
 
       h.each do |k, v|
         k = k.to_s if !k.respond_to?(:to_sym) && k.respond_to?(:to_s)
-        s.new_ostruct_member(k)
+        s.send(:new_ostruct_member!, k)
 
         if v.is_a?(Hash)
           v = v['type'] == 'hash' ? v['contents'] : __convert(v)
