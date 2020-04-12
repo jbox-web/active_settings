c7f47a81c993980341978d37436b4f51286ca95d
Nicolas Rodriguez <nicoladmin@free.fr>
2020-04-13 01:02:40 +0200

    Add default values to mattr_accessor

--- lib/active_settings.rb
+++ lib/active_settings.rb
@@ -17,20 +17,16 @@ loader.setup
 module ActiveSettings
 
   # ActiveSettings options
-  mattr_accessor :fail_on_missing, :use_env, :env_separator, :env_prefix, :env_converter, :env_parse_values
-
-  @@fail_on_missing  = false
-  @@use_env          = false
-  @@env_separator    = '.'
-  @@env_prefix       = 'SETTINGS'
-  @@env_converter    = :downcase
-  @@env_parse_values = true
+  mattr_accessor :fail_on_missing,  default: false
+  mattr_accessor :use_env,          default: false
+  mattr_accessor :env_separator,    default: '.'
+  mattr_accessor :env_prefix,       default: 'SETTINGS'
+  mattr_accessor :env_converter,    default: :downcase
+  mattr_accessor :env_parse_values, default: true
 
   # deep_merge options
-  mattr_accessor :knockout_prefix, :merge_nil_values, :overwrite_arrays, :keep_array_duplicates
-
-  @@knockout_prefix       = nil
-  @@overwrite_arrays      = false
-  @@merge_nil_values      = true
-  @@keep_array_duplicates = true
+  mattr_accessor :knockout_prefix,       default: nil
+  mattr_accessor :merge_nil_values,      default: false
+  mattr_accessor :overwrite_arrays,      default: true
+  mattr_accessor :keep_array_duplicates, default: true
 end
